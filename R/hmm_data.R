7633648dfcebb3256d2261b698a5263596b89491
EpiSlim <lotfi.slim91@gmail.com>
2018-11-04 20:04:17 +0100

    Secondary: just to properly prepare R/hmm_data.R for removal

--- R/hmm_data.R
+++ R/hmm_data.R
@@ -1,9 +1,12 @@
-#' Arabidopsis QTL data on gravitropism
+#' fastPHASE hidden Markov model for the genotypes dataset
 #'
-#' Data from a QTL experiment on gravitropism in
-#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
-#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
-#' increments over eight hours.
+#' Since the fastPHASE executable is not part of this package, we instead
+#' provide its output in order to demonstrate the full features of the
+#' epiGWAS package in the accompanying vignette. By means of the wrapper
+#' function \code{\link{fast_HMM}},  we ran fastPHASE with the
+#' paramaterization \code{n_state = 10} and  \code{n_iter = 25}. The
+#' structure of the \code{hmm} list is explained in greater detail in
+#' the documentation of \code{\link[SNPknock]{SNPknock.fp.loadFit_hmm}}
 #'
 #' @docType data
 #'
@@ -11,7 +14,8 @@
 #'
 #' @name hmm
 #'
-#' @format An object of class \code{"cross"}
+#' @format A list consisting of three fields: \code{pInit}, \code{Q} and
+#' \code{pEmit}
 #'
 #' @keywords datasets
 #'
@@ -20,6 +24,11 @@
 #'     to inferring missing genotypes and haplotypic phase. American Journal of
 #'     Human Genetics, 78(4), 629–644. https://doi.org/10.1086/502802
 #'
+#' @seealso \code{\link{fast_HMM}} and \code{\link[SNPknock]{SNPknock.fp.loadFit_hmm}}
+#'
 #' @examples
 #' data(hmm)
+#' setequal(names(hmm), c("pEmit", "Q", "pInit"))
 "hmm"
+4
+4
